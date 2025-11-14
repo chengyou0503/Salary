d435748beb9ced4e31f3fba763bc48e05b3fb3bc
chengyou0503 <99228903+chengyou0503@users.noreply.github.com>
2025-11-14 10:20:11 +0800

    fix: Remove disclaimer from App.vue and Reset button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app>
     <router-view></router-view>
-    <br><br>
-    <small class="float-right">*本程式非中國信託官方工具，純屬方便個人編輯使用。</small>
   </v-app>
 </template>
 
--- src/views/BatchFile.vue
+++ src/views/BatchFile.vue
@@ -111,7 +111,6 @@
         <v-icon small @click="deleteItem(item)"> fas fa-trash </v-icon>
       </template>
       <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize"> Reset </v-btn>
       </template>
     </v-data-table>
     <br>
